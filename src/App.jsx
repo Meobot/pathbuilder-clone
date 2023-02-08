b1381fc126161911fff308bef196624c4e3415b3
Meobot <nathanrhodes312@gmail.com>
2023-02-08 13:25:38 -0500

    project start

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import React from 'react'
+
+//TODO Menu bar component
+//TODO Character traits component
+//TODO Char. stats component
+//TODO Conditions component
+//TODO Skills component
+//TODO Inventory component
+//TODO Feats component
+
+function App() {
+
+  return (
+	<div className="App">
+
+	</div>
+  )
+}
+
+export default App
