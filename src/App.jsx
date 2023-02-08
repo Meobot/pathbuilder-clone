cbc594cc50a6da47294de6f54f50d578f8179ea4
Meobot <nathanrhodes312@gmail.com>
2023-02-08 13:47:54 -0500

    App template

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react'
 import React from 'react'
 
-//TODO Menu bar component
-//TODO Character traits component
-//TODO Char. stats component
-//TODO Conditions component
-//TODO Skills component
-//TODO Inventory component
-//TODO Feats component
-
 function App() {
+
+	//TODO Char constructor
 
   return (
 	<div className="App">
-
+		<div className="Menu-container">
+			{/* Menu component */}
+		</div>
+		<div className="main-container">
+			<div className="main-column-left">
+				//TODO Character traits component
+				//TODO Feats component
+				//TODO Combine traits / feats into Build component
+				{/* Build component*/}
+			</div>
+			<div className="main-column-right">
+				<div className="main-right-upper">
+					<div className="Stats-container">
+						{/* Stats component*/}
+					</div>
+					<div className="Conditions-container">
+						{/* Conditions component */}
+					</div>
+				</div>
+				<div className="main-right-lower">
+					<div className="Skills-container">
+						{/* Skills component */}
+					</div>
+					<div className="Inventory-container">
+						{/* Inventory component */}
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
   )
 }
